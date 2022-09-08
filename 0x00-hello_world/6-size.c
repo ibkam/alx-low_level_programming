0b0526bcde5b50669b1a8263bc2582b8f646abb0
ibkam <ibrahimkamau254@gmail.com>
2022-09-08 15:32:03 -0700

    Task 5

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * DEscription: using size of to print the size of various types.
+ *
+ * Return: Always 0 (succesful)
+ */
+
+int main (void)
+{
+
+	/* sizeof evaluates the size of a variable */
+	printf

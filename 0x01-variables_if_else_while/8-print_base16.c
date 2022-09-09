6e414d0e86405dcbf83b190fe5b549901f82c9b1
ibkam <ibrahimkamau254@gmail.com>
2022-09-09 16:06:24 -0700

    Task 8

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -13,7 +13,7 @@ int main(void)
 	for (var = 0; var < 10; var++)
 		putchar((var % 10) + '0');
 
-	for (var1 = 'a'; var1 <= 'f'; var++)
+	for (var1 = 'a'; var1 <= 'f'; var1++)
 		putchar(var1);
 
 	putchar('\n');

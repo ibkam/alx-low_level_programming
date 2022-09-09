aacff74af1258c3bcd1c9efa6bf33120901a6946
ibkam <ibrahimkamau254@gmail.com>
2022-09-08 17:51:53 -0700

    Task 6

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -15,11 +15,11 @@ int main(void)
 	char d;
 	float f;
 	/*sizeof evaluates the size of a variable */
-	fprintf("size of a char: %lu byte(s)\n", (unsigned long)sizeof(d));
-	fprintf("size of an int: %lu byte(s)\n", (unsigned long)sizeof(a));
-	fprintf("size of a long int: %lu byte(s)\n", (unsigned long)sizeof(b));
-	fprintf("size of a long long int: %lu byte(s)\n", (unsigned long)sizeof(c));
-	fprintf("size of a float: %lu byte(s)\n", (unsigned long)sizeof(f));
+	printf("size of a char: %lu byte(s)\n", (unsigned long)sizeof(d));
+	printf("size of an int: %lu byte(s)\n", (unsigned long)sizeof(a));
+	printf("size of a long int: %lu byte(s)\n", (unsigned long)sizeof(b));
+	printf("size of a long long int: %lu byte(s)\n", (unsigned long)sizeof(c));
+	printf("size of a float: %lu byte(s)\n", (unsigned long)sizeof(f));
 
 	return (0);
 

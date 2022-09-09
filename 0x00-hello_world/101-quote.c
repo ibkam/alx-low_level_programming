ed4e31cd4639b49494e9a2077eee964eff4ae4c2
ibkam <ibrahimkamau254@gmail.com>
2022-09-09 05:43:16 -0700

    Task 8

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <unistd.h>
 
 /**
  * *main - Entry point
@@ -11,7 +12,8 @@
 
 int main(void)
 {
-		char quo[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
-
-		write(1, quo, 59);
-		return (1);
+	char var[] = "and that piece of art is useful\" - Dora korpar, 2015-10-19\n"
+	write(2, var, sizeof(var) - 1);
+	return (1);
+}
+		

d70732558550e39d147e3cea444e47790572291e
ibkam <ibrahimkamau254@gmail.com>
2022-09-09 06:15:08 -0700

    Task 8

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,18 +1,23 @@
 #include <stdio.h>
+
 #include <unistd.h>
 
+
+
 /**
- * *main - Entry point
+ * main - Entry point
  *
  * Description: print a quote using write function
  * ssize_t write(int fd, const void *buf, size_t count);
  *
- * Return: This time return an error 1.
+ * Return: should return 1 (sucess)
  */
 
 int main(void)
+
 {
-	char var[] = "and that piece of art is useful\" - Dora korpar, 2015-10-19\n"
+	char var[] = "and that piece of art is useful\" - Dora korpar, 2015-10-19\n";
+
 	write(2, var, sizeof(var) - 1);
 	return (1);
 }

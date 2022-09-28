46da3d0c3caf51d04dc9cf05e2b041b44078aeb3
ibkam <ibrahimkamau254@gmail.com>
2022-09-28 12:17:33 -0700

    Task 0

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * _puts_recursion - prints a string recursively
+ * @s: pointer to the string
+ *
+ */
+void _puts_recursion(char *s)
+{
+	if(strlen(s) < 1)
+		_putchar('\n');
+	else
+	{
+		_putchar(*s);
+		_puts_recursion(s + 1);
+	}
+}
---
+++ 0x08-recursion/README.md
@@ -0,0 +1,2 @@
+0x08. C - Recursion
+Recursion is the process of repeating items in a self-similar way.ecursive call of the function allows a program to call a function inside the same function.

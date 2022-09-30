1bf7ba25246998f5cffbfd8dbfdbf8661c19ba71
ibkam <ibrahimkamau254@gmail.com>
2022-09-30 10:42:18 -0700

    Task 1

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - main function
+ * @argc: number of command line arguments
+ * @argv: array containing the program command line arguments
+ *
+ * Return: 0 if successful
+ */
+
+int main(int argc, char *argv[])
+{
+	int count = 0;
+
+	while (argc > 1)
+		count++;
+
+	(void)*argv;
+
+	printf("%d\n", count);
+	exit(EXIT_SUCCESS);
+}

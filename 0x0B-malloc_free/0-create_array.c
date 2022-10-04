7bd4d5e870c206a91f855783905365d9f1028417
ibkam <ibrahimkamau254@gmail.com>
2022-10-04 08:01:08 -0700

    Task 0

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -13,16 +13,14 @@
  */
 char *create_array(unsigned int size, char c)
 {
-	char array;
+	char *array_alloc = malloc(size * sizeof(char));
 	unsigned int i;
 
-	if (size == 0)
+	if (size == 0 || array_alloc == NULL)
 		return (NULL);
-	array = malloc(size of(char) * size);
 
-	if (array == NULL)
-		return (NULL);
 	for (i = 0; i < size; i++)
-		array[i] = c;
-	return (array);
+		array_alloc[i] = c;
+
+	return(array_alloc);
 }

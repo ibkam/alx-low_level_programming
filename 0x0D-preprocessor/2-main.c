6b195220a0d814e38abdda701f53bbd0bc1fb3d9
ibkam <ibrahimkamau254@gmail.com>
2022-10-11 14:16:05 -0700

    Task 2

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#define C "2.main.h"
+
+/**
+ * main - prints the name of the file it was compiled
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	printf("%s\n", C);
+	return (0);
+}

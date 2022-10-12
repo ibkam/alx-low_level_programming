b4c6dceeae0dad7614665140348308911afcd5c6
ibkam <ibrahimkamau254@gmail.com>
2022-10-12 12:00:13 -0700

    Task 0

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,16 @@
+#ifndef DOG_H_
+#define DOG_H_
+
+/**
+ * struct dog - structure definition of a dog
+ * @name: character string
+ * @age: age interger
+ * @owner: character string
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+#endif

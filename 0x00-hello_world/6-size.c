eef2528372dd1542ba403132f690f8c13c82be1c
ibkam <ibrahimkamau254@gmail.com>
2022-10-18 08:15:05 -0700

    sizeof

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -15,6 +15,6 @@ int main(void)
 	printf("size of a long int: %d byte(s)\n", sizeof(long int));
 	printf("size of a long long int: %d byte(s)\n", sizeof(long long int));
 	printf("size of a float: %d byte(s)\n", sizeof(float));
-	fprintf(stderr, "%s", "Anything\n");
+	fprintf("%s", "Anything\n");
 	return (0);
 }

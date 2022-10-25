67c5fb01422dd0e83e0ef1b8438da18fcd2ca654
ibkam <ibrahimkamau254@gmail.com>
2022-10-25 14:59:38 -0700

    singly list

--- 0x12-singly_linked_lists/3-add_node_end.c
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -9,12 +9,14 @@
 list_t *add_node_end(list_t **head, const char *str)
 {
 	list_t *current_node;
-	list_t *new_node = malloc(sizeof(list_t));
-	current_node = *head;
+	list_t *new_node;
 
+	current_node = *head;
+	if (head == NULL)
+		return (NULL);
+	new_node = malloc(sizeof(list_t));
 	if (new_node == NULL)
 		return (NULL);
-
 	new_node->str = strdup(str);
 	new_node->len = strlen(str);
 	new_node->next = NULL;
@@ -23,12 +25,10 @@ list_t *add_node_end(list_t **head, const char *str)
 	if (*head == NULL)
 	{
 		*head = new_node;
-		return (new_node);
+		return (*head);
 	}
-	while (current_node->next)
-	       current_node = current_node->next;
-	current_node->next = new_node;
-
+	while (current_node->next != NULL)
+		current_node = current_node->next;
+	current_node->next =new_node;
 	return (*head);
 }
-

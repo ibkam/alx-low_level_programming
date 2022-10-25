52ef46e13409bb9bbf4058c96f60051e23cec130
ibkam <ibrahimkamau254@gmail.com>
2022-10-25 14:10:14 -0700

    singly list

---
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -0,0 +1,34 @@
+#include "lists.h"
+/**
+ * add_node_end - adds a new node at the end of the list
+ * @head: pointer to the first node in the list
+ * @str: string to include in the node
+ * Return: a pointer to the head of the list
+ */
+
+list_t *add_node_end(list_t **head, const char *str)
+{
+	list_t *current_node;
+	list_t *new_node = malloc(sizeof(list_t));
+	current_node = *head;
+
+	if (new_node == NULL)
+		return (NULL);
+
+	new_node->str = strdup(str);
+	new_node->len = strlen(str);
+	new_node->next = NULL;
+	return (new_node);
+
+	if (*head == NULL)
+	{
+		*head = new_node;
+		return (new_node);
+	}
+	while (current_node->next)
+	       current_node = current_node->next;
+	current_node->next = new_node;
+
+	return (*head);
+}
+

be2416e3149def08e62f52905ae655e48d077ca0
ibkam <ibrahimkamau254@gmail.com>
2022-10-25 16:21:06 -0700

    function executed before main

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+void myStartupFun(void)__attribute__((constructor));
+
+/**
+ * mystartupFun - Functions that are executed before main
+ *
+ */
+void myStartupFun(void)
+{
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
+}

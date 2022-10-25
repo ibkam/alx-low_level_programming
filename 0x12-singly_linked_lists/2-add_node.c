76695e84a6f1d78204704becfe70fb559d5b9b1b
ibkam <ibrahimkamau254@gmail.com>
2022-10-24 17:18:01 -0700

    singly list

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -16,7 +16,7 @@ list_t *add_node(list_t **head, const char *str)
 	if (new_node == NULL)
 		return (NULL);
 	if (*head == NULL)
-		return (NULL);
+		new_node->next =NULL;
 	else
 		new_node->next = *head;
 	new_node->str = strdup(str);

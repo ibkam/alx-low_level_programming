aa7da44260def0a911b2483e55a3c40b86ca2fc1
ibkam <ibrahimkamau254@gmail.com>
2022-10-25 15:36:58 -0700

    singly list

---
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -0,0 +1,23 @@
+#include "lists.h"
+
+/**
+ * free_list - free the list
+ * @head; pointer to the first node of the list
+ */
+
+void free_list(list_t *head)
+{
+	list_t *current_node;
+
+	if (head == NULL)
+		return;
+	while (head->next != NULL)
+	{
+		current_node = head->next;
+		free(head->str);
+		free(head);
+		head = current_node;
+	}
+	free(head->str);
+	free(head);
+}

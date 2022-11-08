6a96778f286c48b50537dc6660e92160566e9999
ibkam <ibrahimkamau254@gmail.com>
2022-11-08 09:26:06 -0800

    convert binary to intint

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -12,21 +12,25 @@
 
 unsigned int binary_to_uint(const char *b)
 {
-	unsigned int i, base = 1, len = 0;
+	unsigned int base = 1, len = 0;
 	unsigned int res = 0;
 
 	if (b == NULL)
 		return (0);
 
-	len = strlen(b);
-
-	for (i = len - 1; i > 0; i--)
+	while (*(b + len))
 	{
-		if (b[i] < '0' || b[i] > '1')
+		len++;
+	}
+
+	while (len)
+	{
+		if (b[len - 1] != '0' && b[len - 1] != '1')
 			return (0);
-		if (b[i] == '1')
+		if (b[len - 1] == '1')
 			res += base;
 		base *= 2;
+		len--;
 	}
 	return (res);
 }

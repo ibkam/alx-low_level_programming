cbb1b2f8d481a15686b8add253a4d70d538ce1c1
ibkam <ibrahimkamau254@gmail.com>
2022-11-09 13:26:11 -0800

    flip number

---
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -0,0 +1,24 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * flip_bits - returns the number of bits after flip
+ * @n: the first bit
+ * @m: the second bit
+ *
+ * Return: the number of bits after flip
+ */
+unsigned int flip_bits(unsigned long int n, unsigned long int m)
+{
+	unsigned long int result;
+	unsigned long int count;
+
+	result = n ^ m;
+	for (count = 0; result > 0;)
+	{
+		if ((result & 1) == 1)
+			count++;
+		result = result >> 1;
+	}
+	return (count);
+}

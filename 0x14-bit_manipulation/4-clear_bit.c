9a644bc83bb11e77d99257f2d237726913b36a85
ibkam <ibrahimkamau254@gmail.com>
2022-11-09 13:28:06 -0800

    set bit at index

--- 0x14-bit_manipulation/4-clear_bit.c
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -3,7 +3,7 @@
 
 /**
  * clear_bit - sets the value of a bit to 0 at a given index
- * @*n: pointer to the number
+ * @n: pointer to the number
  * @index: given poistion
  *
  * Return: 1 if success or -1 if error

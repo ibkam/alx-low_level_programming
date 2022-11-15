bc153d99c3bd80da2d0cb98bec95518ce8cf402b
ibkam <ibrahimkamau254@gmail.com>
2022-11-15 11:59:51 -0800

    Read and print

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -22,10 +22,20 @@ int create_file(const char *filename, char *text_content)
 	}
 
 	/*if text_content is NULL, create an empty file*/
-	o = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
+	o = open(filename, O_CREAT | O_EXCL | O_WRONLY, 0600);
+	if (o == -1)
+	{
+		if (errno == EEXIST)
+		{
+			o = open(filename, O_WRONLY | O_TRUNC);
+			if (o == -1)
+				return (-1);
+		}
+		else
+			return (-1);
+	}
 	w = write(0, text_content, len);
-
-	if (o == -1 || w == -1)
+	if (w == -1)
 		return (-1);
 
 	close(o);

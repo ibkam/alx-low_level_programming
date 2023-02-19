6f7f7dc342efed119d7c474708dbca3462b9e08f
ibkam <ibrahimkamau254@gmail.com>
2023-02-19 09:39:17 -0800

    dynamiclib

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-wget -P /tmp https://github.com/ibkam/alx-low_level_programming/raw/master/0x18-dynamic_libraries/libinj.so
-export LD_PRELOAD=/tmp/libinj.so
+wget -P /tmp https://github.com/ibkam/alx-low_level_programming/blob/master/0x18-dynamic_libraries/libwin.so
+export LD_PRELOAD=/tmp/libwin.so

71a8903bc41b445824d3673d92c623fb3e6eba12
ibkam <110990848+ibkam@users.noreply.github.com>
2023-10-30 13:44:26 +0300

    Update 5-flip_bits.c

--- 0x14-bit_manipulation/5-flip_bits.c
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -1,6 +1,5 @@
 #include "main.h"
 
-
 /**
  * flip_bits - returns the number of bits after flip
  * @n: the first bit
@@ -10,16 +9,19 @@
  */
 unsigned int flip_bits(unsigned long int n, unsigned long int m)
 {
-	unsigned int count;
-	unsigned int result;
+	unsigned long int result;
+	unsigned long int count;
 
-	result = n ^ m;
 	result = n ^ m;
 	for (count = 0; result > 0;)
 	{
 		if ((result & 1) == 1)
 			count++;
 		result = result >> 1;
+	}
+	return (count);
+}
+
 	}
 	return (count);
 }
